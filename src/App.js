2040f16d06788f1d90de425ddbed960b8cfd7461
Jan Steeg <jan.n.steeg@student.fh-kiel.de>
2018-11-18 21:38:13 +0100

    converted to redux

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import HelloWorld from "./HelloWorld";
+import { store } from "./store";
+import ButtonGroup from "./ButtonGroup";
+
+class App extends Component {
+  render() {
+    return [
+      <HelloWorld tech={store.getState().tech} />,
+      <ButtonGroup key={2} technologies={["React", "Elm", "React-redux"]} />
+    ]
+  }
+}
+
+export default App;
